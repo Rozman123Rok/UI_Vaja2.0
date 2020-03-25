fcb8c0ccb48f60aa6bc6a1025770619c93662a0f
Rok Rozman <rok.rozman3@gmail.com>
2020-03-25 18:05:42 +0100

    Dodal gumbe in ko kliknes se oznacijo in dodajo v polje

---
+++ Vaja2.0/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Vaja2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button1.Location = new System.Drawing.Point(85, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 117);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button2.Location = new System.Drawing.Point(221, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 117);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button3.Location = new System.Drawing.Point(354, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 117);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button4.Location = new System.Drawing.Point(85, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 117);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button5.Location = new System.Drawing.Point(221, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 117);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button6.Location = new System.Drawing.Point(354, 178);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(140, 117);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button7.Location = new System.Drawing.Point(85, 291);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(140, 117);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button8.Location = new System.Drawing.Point(221, 291);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(140, 117);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button9.Location = new System.Drawing.Point(354, 291);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(140, 117);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Igralec - 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(691, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "AI - 0";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(629, 64);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(629, 87);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(629, 110);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(629, 133);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton4_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(629, 156);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 15;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Click += new System.EventHandler(this.radioButton5_Click);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(629, 179);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 16;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.Click += new System.EventHandler(this.radioButton6_Click);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(629, 202);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 17;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.Click += new System.EventHandler(this.radioButton7_Click);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(629, 225);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(31, 17);
+            this.radioButton8.TabIndex = 18;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.Click += new System.EventHandler(this.radioButton8_Click);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(629, 248);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 19;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.Click += new System.EventHandler(this.radioButton9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Igra vs AI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+    }
+}
+
---
+++ Vaja2.0/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vaja2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    MessageBox.Show(" test: " + tabela[i, j]);
+                }
+            }
+            */
+        }
+        
+        
+        int MAX_globina = 1; /// do katere globine gremo
+        public enum Igralec
+        {
+            /// znaki za igralce
+            X, O
+        }
+
+        Igralec trenutniIgralec;
+
+        int[,] tabela = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } }; /// moje polje kjer bomo devali X O
+        // X =1; O=2;
+
+
+
+
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 1;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 2;
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 3;
+        }
+
+        private void radioButton4_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 4;
+        }
+
+        private void radioButton5_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 5;
+        }
+
+        private void radioButton6_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 6;
+        }
+
+        private void radioButton7_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 7;
+        }
+
+        private void radioButton8_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 8;
+        }
+
+        private void radioButton9_Click(object sender, EventArgs e)
+        {
+            MAX_globina = 9;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tabela[0, 0] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button1.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button1.Enabled = false; /// da ga nemores ponovno stisnet
+                button1.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[0, 0] = 1;
+                //MessageBox.Show("Tabela: " + tabela[0, 0] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tabela[0, 1] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button2.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button2.Enabled = false; /// da ga nemores ponovno stisnet
+                button2.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[0, 1] = 1;
+                //MessageBox.Show("Tabela: " + tabela[0, 1] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tabela[0, 2] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button3.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button3.Enabled = false; /// da ga nemores ponovno stisnet
+                button3.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[0, 2] = 1;
+                //MessageBox.Show("Tabela: " + tabela[0, 2] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tabela[1, 0] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button4.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button4.Enabled = false; /// da ga nemores ponovno stisnet
+                button4.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[1, 0] = 1;
+                //MessageBox.Show("Tabela: " + tabela[1, 0] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (tabela[1, 1] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button5.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button5.Enabled = false; /// da ga nemores ponovno stisnet
+                button5.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[1, 1] = 1;
+                //MessageBox.Show("Tabela: " + tabela[1, 1] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (tabela[1, 2] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button6.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button6.Enabled = false; /// da ga nemores ponovno stisnet
+                button6.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[1, 2] = 1;
+                //MessageBox.Show("Tabela: " + tabela[1, 2] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (tabela[2, 0] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button7.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button7.Enabled = false; /// da ga nemores ponovno stisnet
+                button7.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[2, 0] = 1;
+                //MessageBox.Show("Tabela: " + tabela[2, 0] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (tabela[2, 1] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button8.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button8.Enabled = false; /// da ga nemores ponovno stisnet
+                button8.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[2, 1] = 1;
+                //MessageBox.Show("Tabela: " + tabela[2, 1] + " max_globina: " + MAX_globina);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (tabela[2, 2] == 0)
+            {
+                trenutniIgralec = Igralec.X; /// nastavi igralca na X
+                button9.Text = trenutniIgralec.ToString(); // spremenim text gumba na X
+                button9.Enabled = false; /// da ga nemores ponovno stisnet
+                button9.BackColor = System.Drawing.Color.Cyan; /// spremenim barvo gumba
+                tabela[2, 2] = 1;
+                //MessageBox.Show("Tabela: " + tabela[2, 2] + " max_globina: " + MAX_globina);
+            }
+        }
+    }
+}
